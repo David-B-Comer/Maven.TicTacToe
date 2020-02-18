d5cf6d58bc4020c879485132abbc23f1ca387cc5
David Comer <50469980+David-B-Comer@users.noreply.github.com>
2020-02-17 19:54:43 -0500

    Done

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -4,23 +4,60 @@ package rocks.zipcodewilmington.tictactoe;
  * @author leon on 6/22/18.
  */
 public class Board {
+    private Character[][] board;
     public Board(Character[][] matrix) {
+        this.board = matrix;
+    }
+
+    public Boolean checkVertical(Character XO) {
+        for (int i = 0; i <= 2; i++) {
+            if ((board[0][i] == XO) && (board[1][i] == XO) && (board[2][i] == XO)) {
+                return true;
+            }
+        }return false;
+    }
+
+    public Boolean checkHorizontal(Character XO) {
+        for (int i = 0; i <= 2; i++) {
+            if ((board[i][0] == XO) && (board[i][1] == XO) && (board[i][2] == XO)) {
+                return true;
+            }
+        } return false;
+    }
+
+    public Boolean checkDiagonal(Character XO) {
+        for (int i = 0; i <= 2; i++) {
+            if ((board[0][0] == XO && board[1][1] == XO && board[2][2] == XO) || board[0][2] == XO && board[1][1] == XO && board[2][0] == XO)  {
+                return true;
+            }
+        } return false;
     }
 
     public Boolean isInFavorOfX() {
-        return null;
+        if (checkHorizontal('X') || checkVertical('X') || checkDiagonal('X')) {
+            return true;
+        } return false;
     }
 
     public Boolean isInFavorOfO() {
-        return null;
+        if (checkHorizontal('O') || checkVertical('O') || checkDiagonal('O')) {
+            return true;
+        } return false;
     }
 
     public Boolean isTie() {
-        return null;
+        return isInFavorOfX() == isInFavorOfO();
     }
 
     public String getWinner() {
-        return null;
+
+    if (isInFavorOfO() == true) {
+        return "O";
+    }
+    if (isInFavorOfX() == true) {
+        return "X";
+    }
+    return "";
     }
 
 }
